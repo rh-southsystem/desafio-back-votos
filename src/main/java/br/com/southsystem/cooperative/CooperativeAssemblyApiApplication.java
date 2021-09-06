2cc164d972e5ead4ea66e5acce80dc4f20a7aa12
Vinicius Cavalcanti <viniciuscdst@gmail.com>
2021-09-06 14:36:34 -0300

    First Commit - Spring boot structure and some dependencies that will be used

---
+++ src/main/java/br/com/southsystem/cooperative/CooperativeAssemblyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.cooperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeAssemblyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeAssemblyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/southsystem/cooperative/CooperativeAssemblyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.cooperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeAssemblyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
