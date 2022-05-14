0247f72cc4b9c20576b2c2385cd4085e8af790a7
Jorge Silva <jorge.do.carmo123@gmail.com>
2022-05-14 16:04:05 -0300

    Project creation

---
+++ src/main/java/com/southsystem/DesafioBackVotosApplication.java
@@ -0,0 +1,13 @@
+package com.southsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackVotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackVotosApplication.class, args);
+	}
+
+}
