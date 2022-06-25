87a6d94901bd6e75ea958b086b2a63e0c7ac56c8
Victor de Nápoles <victor.santos@valemobi.com.br>
2022-06-25 12:22:26 -0300

    Commit inicial

---
+++ src/main/kotlin/br/com/southsystem/desafiobackvotos/DesafioBackVotosApplication.kt
@@ -0,0 +1,11 @@
+package br.com.southsystem.desafiobackvotos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioBackVotosApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioBackVotosApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/southsystem/desafiobackvotos/DesafioBackVotosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.southsystem.desafiobackvotos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioBackVotosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
